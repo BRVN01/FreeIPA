7ac0c85e53a84a1abb3caaa8e040fb471259b267
BRVN01 <39762901+BRVN01@users.noreply.github.com>
2023-03-29 14:27:57 -0300

    Update NotifyExpirationPassword.sh

--- NotifyExpirationPassword.sh
+++ NotifyExpirationPassword.sh
@@ -1,30 +1,31 @@
-#!/bin/bash
-
-# ipa user-mod --password-expiration=2023-05-22Z fulano
-# ipa user-mod --password-expiration='2023-05-22 18:35:10Z' fulano
+#!/usr/bin/bash
 
 init() {
 #    declare -a days
 #    declare -a expiration
-#    position=0
+    position=0
 
-    export log_file='/var/log/idm_expiration_notify.log'
+    export log_file='/var/log/idm_notify_expiration.log'
 
     # Dias para notificar (60, 30, 14, 3 e 1):
-    days="$(date -d "+60 day" +%Y%m%d) $(date -d "+30 day" +%Y%m%d) $(date -d "+14 day" +%Y%m%d) $(date -d "+3 day" +%Y%m%d) $(date -d "+1 day" +%Y%m%d)"
-    users=$(ldapsearch -x -w $(cat /root/.confignotify.conf) -D uid=usertest,cn=sysaccounts,cn=etc,dc=maddogs,dc=br -H ldaps://idm.maddogs.br uid=* uid givenName mail krbPasswordExpiration)
+    days="$(date -d "+60 day" +%Y%m%d) $(date -d "+30 day" +%Y%m%d) $(date -d "+14 day" +%Y%m%d) $(date -d "+3 day" +%Y%m%d) $(date -d "+1 day" +%Y%m%d)" 
+#    users=$(ldapsearch -x -w $(cat /root/.confignotify.conf) -D uid=usertest,cn=sysaccounts,cn=etc,dc=maddogs,dc=br -H ldaps://idm.maddogs.br uid=* uid givenName mail krbPasswordExpiration)
 
-#   Another way:
-#   kdestroy
-#   kinit -k -t FILENAME USERNAME
-#   users$(ldapsearch -Y GSSAPI -b "cn=users,cn=accounts,dc=maddogs,dc=br"  uid=* uid givenName mail krbPasswordExpiration)
-#   kdestroy
+    # Usando keytab:
+    kdestroy
+    kinit -k -t /keytab.file USERNAME
+    users=$(ldapsearch -LLL -b "cn=users,cn=accounts,dc=maddogs,dc=br"  'uid=*' uid givenName mail krbPasswordExpiration 2>/dev/null)
+    kdestroy
+
 
     for day in ${days[@]}; do
 
-        if [[ $(grep -B1 "${day}" <<<"${users}") ]]; then
-            expiration[$position]="$(grep -B3 "${day}" <<<${users} | sed 's/uid: //g; s/givenName: //g; s/mail: //g; s/krbPasswordExpiration: //g' | paste -sd' ')"
-            position=$((++position))
+        if [[ $(grep "${day}" <<<"${users}") ]]; then
+
+            for quantity in $(seq 1 $(grep -c "${day}" <<<"${users}")); do
+                expiration[$position]="$(grep -m${quantity} -B3 "${day}" <<<"${users}" | tail -n4 | sed 's/uid: //g; s/givenName: //g; s/mail: //g; s/krbPasswordExpiration: //g' | paste -sd' ')"
+                position=$((++position))
+            done
         fi
     done
 
@@ -43,15 +44,15 @@ show() {
 }
 
 show_verbose() {
-    template=$(source "/root/.template")
+    template=$(source "template")
     [[ $? -ne 0 ]] && echo "Erro no Script!" && exit 1
 
     for s in ${!expiration[@]}; do
-        echo ${expiration[$s]}
+        #echo ${expiration[$s]}
 
         while read uid name mail date; do
             dateutc=$(date -d "$(echo "${date}" | awk '{print substr($0,1,4)"/"substr($0,5,2)"/"substr($0,7,2)" "substr($0,9,2)":"substr($0,11,2)":"substr($0,13,2)}')" +'%d/%m/%Y %H:%M:%S %Z')
-            template=$(source "/root/.template")
+            template=$(source "template")
 
             echo "${template}" 
         done <<<${expiration[$s]}
@@ -60,16 +61,16 @@ show_verbose() {
 }
 
 send() {
-    template=$(source "/root/.template")
+    template=$(source "template")
     [[ $? -ne 0 ]] && echo "Erro no Script!" >> ${log_file} && exit 1
 
     for s in ${!expiration[@]}; do
 
         while read uid name mail date; do
             dateutc=$(date -d "$(echo "${date}" | awk '{print substr($0,1,4)"/"substr($0,5,2)"/"substr($0,7,2)" "substr($0,9,2)":"substr($0,11,2)":"substr($0,13,2)}')" +'%d/%m/%Y %H:%M:%S %Z')
-            template=$(source "/root/.template")
+            template=$(source "template")
 
-            echo "${template}" | mail -r "noreply@DOMAIN" -s "[NOTIFICATION] Your password will expire soon" ${mail} && echo "[$(date +%Y-%m-%d\ %H:%M:%S)] ${uid}" >> ${log_file}
+#            echo "${template}" | mail -r "noreply@registro.br" -s "[NOTIFICAÇÃO] Sua senha de e-mail vai expirar em breve" ${mail} && echo "[$(date +%Y-%m-%d\ %H:%M:%S)] ${uid}" >> ${log_file}
         done <<<${expiration[$s]}
     done
 }
